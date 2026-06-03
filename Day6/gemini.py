f7067ca05ecf198b3cfdec2920b738dc2514783c
srikanthsrs25 <srikanth.kambham@srsconsultinginc.com>
2026-06-03 17:06:36 +0530

    update code

--- Day6/gemini.py
+++ Day6/gemini.py
@@ -2,19 +2,30 @@ from google import genai
 from dotenv import load_dotenv
 import os
 
-
+# Load environment variables
 load_dotenv()
 
-
+# Get API key
 api_key = os.getenv("GEMINI_API_KEY")
 
+# Create Gemini client
 client = genai.Client(api_key=api_key)
 
-
+# Generate response
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents="Explain Python inheritance in simple words"
 )
 
+# Print AI response
 print("\nAI RESPONSE:\n")
 print(response.text)
+
+# Print token usage
+print("\nTOKEN USAGE:\n")
+
+try:
+    print(response.usage_metadata)
+except:
+    print("Token usage not available")
+
